96d5145798c378da81795ea4fc8c911f38df493c
Peshal61 <devrookie88@gmail.com>
2026-09-23 10:45:18 +0545

    js-class-4::loop learnt

---
+++ oddeven.js
@@ -0,0 +1,24 @@
+let x = 7;
+let y = 8;
+let z = 9;
+
+// Check x
+if (x % 2 === 0) {
+  console.log(`x (${x}) is even`);
+} else {
+  console.log(`x (${x}) is odd`);
+}
+
+// Check y
+if (y % 2 === 0) {
+  console.log(`y (${y}) is even`);
+} else {
+  console.log(`y (${y}) is odd`);
+}
+
+// Check z
+if (z % 2 === 0) {
+  console.log(`z (${z}) is even`);
+} else {
+  console.log(`z (${z}) is odd`);
+}
---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("External script loaded");
+const x = "Pretty";
+console.log(x);
+const a = 5;
+const b = 6;
+const sum = a + b;
+console.log("sum", sum);
